9eefa86246181e4b9d45759f9fd090307adfb0b5
Igor Krasovskiy <mykras@me.com>
2021-11-28 21:08:52 +0200

    Update customer_function.py
    Added function for array.

--- customer_function.py
+++ customer_function.py
@@ -6,7 +6,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from scipy.stats import norm, skew #for some statistics
 
-#
+# For DataFrame
 def printing_distribution_skewness_kurtosis(df, column):
     # Distribution
     sns.set_style("white")
@@ -31,5 +31,25 @@ def printing_distribution_skewness_kurtosis(df, column):
     ax.text(x=1.1, y=0.95, transform=ax.transAxes, s="Kurtosis: %f" % df[column].kurt(),\
     fontweight='demibold', fontsize=10, verticalalignment='top', horizontalalignment='right',\
     backgroundcolor='white', color='xkcd:dried blood')
+
+    plt.show()
+
+ # For Array
+ def printing_array_distribution(array):
+    # Distribution
+    sns.set_style("white")
+    sns.set_color_codes(palette='deep')
+    f, ax = plt.subplots(figsize=(12, 8))
+
+    # Fit a normal distribution
+    mu, std = norm.fit(array)
+
+    # Frequency
+    sns.distplot(array, color="b", fit = stats.norm)
+    ax.xaxis.grid(False)
+    ax.set(ylabel="Frequency")
+    ax.set(xlabel="Currencies")
+    ax.set(title="distribution: mu = %.2f, std = %.2f" % (mu, std))
+    sns.despine(trim=True, left=True)
 
     plt.show()
