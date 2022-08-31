3c808763fa25c89338ccb7b2773242754de8a5fd
Igor Krasovskiy <mykras@me.com>
2022-08-31 17:43:16 +0200

    Update customer_function.py

--- customer_function.py
+++ customer_function.py
@@ -10,6 +10,12 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from scipy.stats import norm, skew #for some statistics
 
+# Plots
+from plotly.offline import iplot
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
 # data quality   
 def data_quality(df, column):  #convert_dtypes_with_reduce_memory(df)
     # datetime
@@ -150,3 +156,141 @@ def create_cohort(df, start_date, end_date):
     cohort = df[(df.index >=start_date) & (df.index <= end_date)].copy()
     #cohort.reset_index(inplace=True, drop=True)
     return(cohort)
+
+
+
+# Helper functions for structured data
+## Get info about the dataset
+def dataset_info(dataset, dataset_name: str):
+    print(f"Dataset Name: {dataset_name} \
+        | Number of Samples: {dataset.shape[0]} \
+        | Number of Columns: {dataset.shape[1]}")
+    print(30*"=")
+    print("Column             Data Type")
+    print(dataset.dtypes)
+    print(30*"=")
+    missing_data = dataset.isnull().sum()
+    if sum(missing_data) > 0:
+        print(missing_data[missing_data.values > 0])
+    else:
+        print("No Missing Data on this Dataset!")
+    print(30*"=")
+    print("Memory Usage: {} MB".\
+         format(np.round(
+         dataset.memory_usage(index=True).sum() / 10e5, 3
+         )))
+## Dataset Sampling
+def data_sampling(dataset, frac: float, random_seed: int):
+    data_sampled_a = dataset.sample(frac=frac, random_state=random_seed)
+    data_sampled_b =  dataset.drop(data_sampled_a.index).reset_index(drop=True)
+    data_sampled_a.reset_index(drop=True, inplace=True)
+    return data_sampled_a, data_sampled_b   
+## Bar Plot
+def bar_plot(data, plot_title: str, x_axis: str, y_axis: str):
+    colors = ["#0080ff",] * len(data)
+    colors[0] = "#ff8000"
+    trace = go.Bar(y=data.values, x=data.index, text=data.values, 
+                    marker_color=colors)
+    layout = go.Layout(autosize=False, height=600,
+                    title={"text" : plot_title,
+                       "y" : 0.9,
+                       "x" : 0.5,
+                       "xanchor" : "center",
+                       "yanchor" : "top"},  
+                    xaxis={"title" : x_axis},
+                    yaxis={"title" : y_axis},)
+    fig = go.Figure(data=trace, layout=layout)
+    fig.update_layout(template="simple_white")
+    fig.update_traces(textposition="outside",
+                    textfont_size=14,
+                    marker=dict(line=dict(color="#000000", width=2)))                
+    fig.update_yaxes(automargin=True)
+    iplot(fig)
+## Plot Pie Chart
+def pie_plot(data, plot_title: str):
+    trace = go.Pie(labels=data.index, values=data.values)
+    layout = go.Layout(autosize=False,
+                    title={"text" : plot_title,
+                       "y" : 0.9,
+                       "x" : 0.5,
+                       "xanchor" : "center",
+                       "yanchor" : "top"})
+    fig = go.Figure(data=trace, layout=layout)
+    fig.update_traces(textfont_size=14,
+                    marker=dict(line=dict(color="#000000", width=2)))
+    fig.update_yaxes(automargin=True)            
+    iplot(fig)
+## Histogram
+def histogram_plot(data, plot_title: str, y_axis: str):
+    trace = go.Histogram(x=data)
+    layout = go.Layout(autosize=False,
+                    title={"text" : plot_title,
+                       "y" : 0.9,
+                       "x" : 0.5,
+                       "xanchor" : "center",
+                       "yanchor" : "top"},  
+                    yaxis={"title" : y_axis})
+    fig = go.Figure(data=trace, layout=layout)
+    fig.update_traces(marker=dict(line=dict(color="#000000", width=2)))
+    fig.update_layout(template="simple_white")
+    fig.update_yaxes(automargin=True)
+    iplot(fig)
+# Particular case: Histogram subplot (1, 2)
+def histogram_subplot(dataset_a, dataset_b, feature_a: str,
+                        feature_b: str, title: str, title_a: str, title_b: str):
+    fig = make_subplots(rows=1, cols=2, subplot_titles=(
+                        title_a,
+                        title_b
+                        )
+                    )
+    fig.add_trace(go.Histogram(x=dataset_a[feature_a],
+                               showlegend=False),
+                                row=1, col=1)
+    fig.add_trace(go.Histogram(x=dataset_b[feature_b],
+                               showlegend=False),
+                              row=1, col=2)
+    fig.update_layout(template="simple_white")
+    fig.update_layout(autosize=False,
+                        title={"text" : title,
+                        "y" : 0.9,
+                        "x" : 0.5,
+                        "xanchor" : "center",
+                        "yanchor" : "top"},  
+                        yaxis={"title" : "<i>Frequency</i>"})
+    fig.update_traces(marker=dict(line=dict(color="#000000", width=2)))
+    fig.update_yaxes(automargin=True)
+    iplot(fig)
+# Calculate scores with Test/Unseen labeled data
+def test_score_report(data_unseen, predict_unseen):
+    le = LabelEncoder()
+    data_unseen["Label"] = le.fit_transform(data_unseen.Churn.values)
+    data_unseen["Label"] = data_unseen["Label"].astype(int)
+    accuracy = accuracy_score(data_unseen["Label"], predict_unseen["Label"])
+    roc_auc = roc_auc_score(data_unseen["Label"], predict_unseen["Label"])
+    precision = precision_score(data_unseen["Label"], predict_unseen["Label"])
+    recall = recall_score(data_unseen["Label"], predict_unseen["Label"])
+    f1 = f1_score(data_unseen["Label"], predict_unseen["Label"])
+
+    df_unseen = pd.DataFrame({
+        "Accuracy" : [accuracy],
+        "AUC" : [roc_auc],
+        "Recall" : [recall],
+        "Precision" : [precision],
+        "F1 Score" : [f1]
+    })
+    return df_unseen
+
+# Confusion Matrix
+def conf_mat(data_unseen, predict_unseen):
+    unique_label = data_unseen["Label"].unique()
+    cmtx = pd.DataFrame(
+        confusion_matrix(data_unseen["Label"],
+                         predict_unseen["Label"],
+                         labels=unique_label), 
+        index=['{:}'.format(x) for x in unique_label], 
+        columns=['{:}'.format(x) for x in unique_label]
+    )
+    ax = sns.heatmap(cmtx, annot=True, fmt="d", cmap="YlGnBu")
+    ax.set_ylabel('Predicted')
+    ax.set_xlabel('Target');
+    ax.set_title("Predict Unseen Confusion Matrix", size=14);
